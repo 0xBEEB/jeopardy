58d1d7312e7be10e8fa1269f5f557db599ccd64c
Thomas Schreiber <ubiquill@gmail.com>
2011-01-12 13:41:10 -0800

    I can haz spawned questions!

--- jeopardy.py
+++ jeopardy.py
@@ -5,22 +5,29 @@
 # Copyright 2011 Thomas Schreiber <ubiquill@cat.pdx.edu>
 
 import math, sys
-from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
-
-class MainWindow(QtGui.QWidget):
+class QuestWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        self.initUI()
+        QMainWindow.__init__(self)
+        layout = QHBoxLayout()
+        answerLabel = QLabel("The answer is")
+        layout.addWidget(answerLabel)
+        self.widget = QWidget()
+        self.widget.setLayout(layout)
 
-    def spawnQuest(self):
-        Answer = QtGui.QMessageBox.question(self, 'Answer', 'The mascot of Linux.',
-                                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, 
-                                            QtGui.QMessageBox.No)
+        self.setCentralWidget(self.widget)
+        self.setWindowTitle("ANSWER")
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.initUI()
 
 
     def initUI(self):
-        self.setWindowTitle('Hacker Jeopardy')
 
         topics = ['topic1', 'topic2', 'topic3', 'topic4', 'topic5']
 
@@ -31,7 +38,7 @@ class MainWindow(QtGui.QWidget):
                       '500', '500', '500', '500', '500' ]
 
 
-        grid = QtGui.QGridLayout()
+        grid = QGridLayout()
 
         i = 0
         pos = [ (0,0), (0,1), (0,2), (0,3), (0,4),
@@ -43,57 +50,68 @@ class MainWindow(QtGui.QWidget):
                 (6,0), (6,1), (6,2), (6,3), (6,4) ]
 
         for topic in topics:
-            title = QtGui.QLabel(topic)
+            title = QLabel(topic)
             title.setFixedHeight(25)
-            title.setAlignment(QtCore.Qt.AlignHCenter)
+            title.setAlignment(Qt.AlignHCenter)
             grid.addWidget(title, pos[i][0], pos[i][1])
             i = i + 1
 
         for question in questions:
-            button = QtGui.QPushButton(question)
-            self.connect(button, QtCore.SIGNAL('clicked()'), self.spawnQuest)
-            button.setSizePolicy(QtGui.QSizePolicy.Expanding,
-                                 QtGui.QSizePolicy.Expanding)
+            button = QPushButton(question)
+            self.connect(button, SIGNAL('clicked()'), self.spawnQuest)
+            button.setSizePolicy(QSizePolicy.Expanding,
+                                 QSizePolicy.Expanding)
             grid.addWidget(button, pos[i][0], pos[i][1])
             i = i + 1
 
-        self.player1 = QtGui.QPushButton('Player 1')
-        self.connect(self.player1, QtCore.SIGNAL('clicked()'), self.setupPlayerOne)
+        self.player1 = QPushButton('Player 1')
+        self.connect(self.player1, SIGNAL('clicked()'), self.setupPlayerOne)
         self.player1.setFlat(True)
         grid.addWidget(self.player1, pos[i][0], pos[i][1])
         i = i + 2
 
-        self.player2 = QtGui.QPushButton('Player 2')
-        self.connect(self.player2, QtCore.SIGNAL('clicked()'), self.setupPlayerTwo)
+        self.player2 = QPushButton('Player 2')
+        self.connect(self.player2, SIGNAL('clicked()'), self.setupPlayerTwo)
         self.player2.setFlat(True)
         grid.addWidget(self.player2, pos[i][0], pos[i][1])
         i = i + 2
 
-        self.player3 = QtGui.QPushButton('Player 3')
-        self.connect(self.player3, QtCore.SIGNAL('clicked()'), self.setupPlayerThree)
+        self.player3 = QPushButton('Player 3')
+        self.connect(self.player3, SIGNAL('clicked()'), self.setupPlayerThree)
         self.player3.setFlat(True)
         grid.addWidget(self.player3, pos[i][0], pos[i][1])
 
-        self.setLayout(grid)
+        self.widget = QWidget()
+        self.widget.setLayout(grid)
+
+        self.setCentralWidget(self.widget)
+        self.setWindowTitle('Hacker Jeopardy')
 
 
     def setupPlayerOne(self):
-        text, ok = QtGui.QInputDialog.getText(self, 'Player 1', 'Enter name:')
+        text, ok = QInputDialog.getText(self, 'Player 1', 'Enter name:')
         if ok:
             self.player1.setText(str(text))
 
     def setupPlayerTwo(self):
-        text, ok = QtGui.QInputDialog.getText(self, 'Player 2', 'Enter name:')
+        text, ok = QInputDialog.getText(self, 'Player 2', 'Enter name:')
         if ok:
             self.player2.setText(str(text))
 
     def setupPlayerThree(self):
-        text, ok = QtGui.QInputDialog.getText(self, 'Player 3', 'Enter name:')
+        text, ok = QInputDialog.getText(self, 'Player 3', 'Enter name:')
         if ok:
             self.player3.setText(str(text))
 
-app = QtGui.QApplication(sys.argv)
-mw = MainWindow()
-mw.show()
-sys.exit(app.exec_())
+
+    def spawnQuest(self):
+        self.questWindow = QuestWindow()
+        self.questWindow.show()
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    mw = MainWindow()
+    mw.showFullScreen()
+    sys.exit(app.exec_())
 
