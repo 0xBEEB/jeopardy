2cfa5234dd0c6e44454ca872f1436aa23cd98e60
Gregory Haynes <greg@greghaynes.net>
2011-01-16 04:01:36 -0800

    Load game from json file

---
+++ game.py
@@ -0,0 +1,39 @@
+import json
+import os
+import random
+
+class Question(object):
+	def __init__(self, question, value, isDD=False):
+		self.question = question
+		self.value = value
+		self.isDD = isDD
+
+class Game(object):
+	def __init__(self, questions):
+		self.questions = {}
+		for key, value in questions.items():
+			qs = []
+			self.questions[key] = qs
+			i = 1
+			for q in value:
+				qs.append(Question(q, i*100))
+				i = i + 1
+
+class GameLoader(object):
+	def __init__(self, gamesdir='data/games'):
+		self.gamesdir = gamesdir
+
+	def loadGame(self, name=None):
+		if name == None:
+			options = os.listdir(self.gamesdir)
+			name = options[int(random.random() * len(options))]
+		questions_fd = open(self.gamesdir + '/' + name)
+		questions = json.load(questions_fd)
+
+		return Game(questions)
+
+if __name__ == '__main__':
+	loader = GameLoader()
+	game = loader.loadGame()
+	print game.questions
+
--- jeopardy.py
+++ jeopardy.py
@@ -5,8 +5,11 @@
 # Copyright 2011 Thomas Schreiber <ubiquill@cat.pdx.edu>
 
 import math, sys
+import json
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+
+import game
 
 class QuestWindow(QMainWindow):
     def __init__(self):
@@ -24,62 +27,48 @@ class QuestWindow(QMainWindow):
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
+        self.gameLoader = game.GameLoader()
         self.initUI()
-
 
     def initUI(self):
 
-        topics = ['topic1', 'topic2', 'topic3', 'topic4', 'topic5']
-
-        questions = [ '100', '100', '100', '100', '100',
-                      '200', '200', '200', '200', '200',
-                      '300', '300', '300', '300', '300',
-                      '400', '400', '400', '400', '400',
-                      '500', '500', '500', '500', '500' ]
+        game = self.gameLoader.loadGame()
+        questions = game.questions
+        topics = questions.keys()
 
 
         grid = QGridLayout()
 
         i = 0
-        pos = [ (0,0), (0,1), (0,2), (0,3), (0,4),
-                (1,0), (1,1), (1,2), (1,3), (1,4),
-                (2,0), (2,1), (2,2), (2,3), (2,4),
-                (3,0), (3,1), (3,2), (3,3), (3,4),
-                (4,0), (4,1), (4,2), (4,3), (4,4),
-                (5,0), (5,1), (5,2), (5,3), (5,4),
-                (6,0), (6,1), (6,2), (6,3), (6,4) ]
-
         for topic in topics:
             title = QLabel(topic)
             title.setFixedHeight(25)
             title.setAlignment(Qt.AlignHCenter)
-            grid.addWidget(title, pos[i][0], pos[i][1])
-            i = i + 1
-
-        for question in questions:
-            button = QPushButton(question)
-            self.connect(button, SIGNAL('clicked()'), self.spawnQuest)
-            button.setSizePolicy(QSizePolicy.Expanding,
-                                 QSizePolicy.Expanding)
-            grid.addWidget(button, pos[i][0], pos[i][1])
-            i = i + 1
+            grid.addWidget(title, 0, i)
+            j = 1
+            for question in questions[topic]:
+                button = QPushButton(str(question.value))
+                self.connect(button, SIGNAL('clicked()'), self.spawnQuest)
+                button.setSizePolicy(QSizePolicy.Expanding,
+                                     QSizePolicy.Expanding)
+                grid.addWidget(button, j, i)
+                j = j + 1
+            i = i + 1     
 
         self.player1 = QPushButton('Player 1')
         self.connect(self.player1, SIGNAL('clicked()'), self.setupPlayerOne)
         self.player1.setFlat(True)
-        grid.addWidget(self.player1, pos[i][0], pos[i][1])
-        i = i + 2
+        grid.addWidget(self.player1, 6, 0)
 
         self.player2 = QPushButton('Player 2')
         self.connect(self.player2, SIGNAL('clicked()'), self.setupPlayerTwo)
         self.player2.setFlat(True)
-        grid.addWidget(self.player2, pos[i][0], pos[i][1])
-        i = i + 2
+        grid.addWidget(self.player2, 6, 2)
 
         self.player3 = QPushButton('Player 3')
         self.connect(self.player3, SIGNAL('clicked()'), self.setupPlayerThree)
         self.player3.setFlat(True)
-        grid.addWidget(self.player3, pos[i][0], pos[i][1])
+        grid.addWidget(self.player3, 6, 4)
 
         self.widget = QWidget()
         self.widget.setLayout(grid)
